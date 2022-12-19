39b175284cce537f7d5f01b9e47d883c386d3668
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-12-19 13:22:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,34 @@
-#include <iostream>
-#include <cmath>
-using namespace std;
+#include <stdio.h>
+#include <math.h>
 
-float f(float y){
-    return (y*y)*cos(y)+1;
-}
-
-int main() {
-       float a=0;
-    float b=0;
-    cout<<"Inserire estremo"<<endl;
-    cin>>a>>b;
-    while((a-b)/2>0.0001){
-        float m=(a+b)/2;
-        if(f(m)==0){
-            a=m;
-            b=m;
-        }
-        else{
-            if(f(m)<0) {
-                a = m;
-            }
-            else{
-                b=m;
-            }
-        }
+int main()
+{
+  double a, b, y, fa, fb, fy, err_su_y;
+  do {
+    printf(" Inserire estremi\n");
+    scanf("%lf", &a);
+    fa = a * a * cos(a) + 1.;
+    scanf("%lf", &b);
+    fb = b * b * cos(b) + 1.;
+    printf("\n");
+  } while (fa * fb >= 0.);
+  do {
+    y = (a + b) * 0.5;
+    fy = y * y * cos(y) + 1.;
+    if( fy == 0. )
+      break;
+    if( fa*fy < 0.0001 ) {
+      b = y;
+      fb = fy;
     }
-    cout<<(a+b)/2<<endl;
+    else {
+      a = y;
+      fa = fy;
+    }
+    err_su_y = fabs( (b-a) * 0.5 );
+  } while(err_su_y >= 1.e-15);
+  printf("%4.3lf\n", y);
+
 }
 
 
