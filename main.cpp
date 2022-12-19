40a2d6da190db1c095ea2c7e218f4dcc6a373ae8
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-12-19 12:46:48 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+
+float f(float y){
+    return (y*y)*cos(y)+1;
+}
+
 int main() {
+       float a=0;
+    float b=0;
+    cout<<"Inserire estremo"<<endl;
+    cin>>a>>b;
+    while((a-b)/2>0.0001){
+        float m=(a+b)/2;
+        if(f(m)==0){
+            a=m;
+            b=m;
+        }
+        else{
+            if(f(m)<0) {
+                a = m;
+            }
+            else{
+                b=m;
+            }
+        }
+    }
+    cout<<(a+b)/2<<endl;
+}
    return 0;
 }
